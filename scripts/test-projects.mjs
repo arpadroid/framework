8f09f55e394951fda3bed02caec141f5961e3598
Andres Vaquero <web.andresvaquero@gmail.com>
2025-03-03 23:09:49 +1100

    adding test all projects

--- scripts/test-projects.mjs
+++ scripts/test-projects.mjs
@@ -1,14 +1,19 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 import { Project } from '@arpadroid/module';
 import { log } from '@arpadroid/module/logger';
+import { hideBin } from 'yargs/helpers';
 import fs from 'fs';
-
+import yargs from 'yargs';
+const argv = yargs(hideBin(process.argv)).argv;
+const PROJECT = argv.project;
 const cwd = process.cwd();
 
 const arpadroid = new Project('framework');
-const projects = arpadroid.getArpadroidDependencies().map(dep => {
-    return new Project(dep, { path: fs.realpathSync(`${cwd}/../${dep}`) });
-});
+const projects =
+    (PROJECT && [new Project(PROJECT)]) ||
+    arpadroid.getArpadroidDependencies().map(dep => {
+        return new Project(dep, { path: fs.realpathSync(`${cwd}/../${dep}`) });
+    });
 
 /**
  * Run tests for all projects.
